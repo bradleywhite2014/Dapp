d1ee01588aab751edf0cd5a41ba925975aa7cf1a
bradleywhite2014 <bradleywhite@Bradleys-MacBook-Pro.local>
2016-02-15 20:18:45 -0600

    Firebase Tutorial Finished
    Set up basic list keeping app using firebase. Its really amazing how
    easy it was.

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,112 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+const Firebase = require('firebase');
+const styles = require('./styles.js')
+
+const StatusBar = require('./Components/StatusBar');
+const ActionButton = require('./Components/ActionButton');
+const ListItem = require('./Components/ListItem');
 'use strict';
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
+  AlertIOS
 } from 'react-native';
 
+
+
 class Dapp extends Component {
+  _renderItem(item) {
+
+  const onPress = () => {
+    AlertIOS.alert(
+      'Complete',
+      null,
+      [
+        {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+        {text: 'Cancel', onPress: (text) => console.log('Cancel')}
+      ],
+      'default'
+    );
+  };
+
+  return (
+    <ListItem item={item} onPress={onPress} />
+  );
+}
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+        <StatusBar title="Dapp" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          style={styles.listview}/>
+
+        <ActionButton title="Add" onPress={this._addItem.bind(this)} />
+
+
       </View>
     );
   }
+
+  constructor(props) {
+    super(props);
+
+    this.itemsRef = new Firebase("amber-inferno-1182.firebaseIO.com/items");
+
+
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key()
+        });
+      });
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+    });
+  }
+
+  _addItem() {
+  AlertIOS.alert(
+    'Add New Item',
+    null,
+    [
+      {
+        text: 'Add',
+        onPress: (text) => {
+          this.itemsRef.push({ title: text })
+        }
+      },
+    ],
+    'plain-text'
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+
+}
 
 AppRegistry.registerComponent('Dapp', () => Dapp);
